2f8e8beac5e044e141509a28a29b1c9e6a1be3ca
João Cerqueira <joaocerqueira65@hotmail.com>
2022-05-11 18:48:27 +0100

    dirty code but no server needed now (click and play)

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-
-import { focusHandler, setTry } from "./handlers.js"
-
 let tryCount = 1
-let gameWord = await getWord()
+let gameWord = ""
 
 function startUp() {
+    getWord()
     setFocus()
     setFocus(1)
     setTry(tryCount)
@@ -12,21 +10,17 @@ function startUp() {
 function setFocus() {
 
     let inputs = document.querySelectorAll(".wordInput")
-
     inputs.forEach(input => {
-
-
         input.addEventListener("keydown", focusHandler)
 
         // next.focus()
     })
 
 }
-
 async function getWord() {
-    let word = await fetch("https://random-word-api.herokuapp.com/word?length=5").then(word => word.json())
+    fetch("https://random-word-api.herokuapp.com/word?length=5").then(word => word.json()).then(word => gameWord = word[0])
 
-    return word[0]
+    //return word[0]
 }
 function checkTry() {
     console.log(gameWord);
@@ -38,7 +32,7 @@ function checkTry() {
 
         word += input.value
 
-
+        if (!input.value) return
         if (gameWordSplit[index] == input.value) {
             input.style.backgroundColor = "green"
             document.getElementById(`${input.value.toUpperCase()}`).style.backgroundColor = "green"
@@ -51,19 +45,76 @@ function checkTry() {
         }
         input.style.backgroundColor = "grey"
         document.getElementById(input.value.toUpperCase()).style.backgroundColor = "grey"
+
     })
     if (word == gameWord) {
-        console.log("game over");
+        gameOver()
         return
     }
     tryCount++
     setTry(tryCount)
 
-    console.log(word.split(""));
+}
+function focusHandler(e) {
+    if (e.keyCode == 13) {
+        document.getElementById("button").click()
+    }
+    if (e.keyCode == 8) {
+        e.target.value = ""
+        e.target.previousElementSibling.focus()
+        e.preventDefault()
+        return
+    }
+
+    if (e.target.maxLength == e.target.value.length) {
+        e.target.nextElementSibling.focus()
+    }
+}
+function setTry(tryCount) {
+    if (tryCount == 7) {
+        console.log("was yout last try");
+        return
+    }
+    let currentRow = document.querySelector(`.column${tryCount}`)
+    //enable try row
+    Array.from(currentRow.children).map(child => {
+        child.disabled = false
+    })
+    currentRow.children[0].focus()
+
+    if (tryCount != 1) {
+
+        let rowBefore = document.querySelector(`.column${tryCount - 1}`)
+        //disable last finished try
+        Array.from(rowBefore.children).map(child => {
+            child.disabled = true
+        })
+    }
+
+}
+function gameOver() {
+    let gameOver = document.getElementById("gameOver")
+    gameOver.style.display = "block"
+    setTimeout(()=>gameOver.style.display = "none",2000)
+}
+function resetGame(){
+    tryCount=1
+    Array.from(document.querySelectorAll(".wordInput")).map(input=>{
+        input.disabled=true
+        input.value=""
+        input.style.backgroundColor=""
+    })
+
+    setTry(tryCount)
+    getWord()
+    let teste=Array.from(document.querySelector(".keyboard").children)
+    teste.map(row=>{
+        Array.from(row.children).map(key=>{
+            key.style.backgroundColor=""
+        })
+    })
+   
 }
 
-
-
-
 startUp()
-document.getElementById("button").addEventListener("click", checkTry, false)
+
